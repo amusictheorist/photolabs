bbfe4d4de08b5076797e468d494ed2d199671c3b
amusictheorist <a_millaresthomson@outlook.com>
2024-11-01 11:45:37 -0400

    Refactoring

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,14 +6,14 @@ const PhotoListItem = (props) => {
   
   return (
     <div className="photo-list__item">
-      <img src={photo.imageSource} alt="user's photo" />
-      <div >
-        <img src={photo.profile} alt="user's profile" />
+      <img className="photo-list__image" src={photo.imageSource} alt="user's photo" />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photo.profile} alt="user's profile" />
         <div className="photo-list__user-info">
-        <span>{photo.username}</span>
-        </div>
-        <div className="photo-list__user-location">
-        <span>{photo.location.city}, {photo.location.country}</span>
+          <span>{photo.username}</span>
+          <div className="photo-list__user-location">
+            <span>{photo.location.city}, {photo.location.country}</span>
+          </div>
         </div>
       </div>
     </div>
