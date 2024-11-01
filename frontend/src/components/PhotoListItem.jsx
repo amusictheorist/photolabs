03cb091068969756e6d230cfb406a89489aa62fe
amusictheorist <a_millaresthomson@outlook.com>
2024-11-01 08:41:21 -0400

    Add styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,18 @@ const PhotoListItem = (props) => {
   const sampleDataForPhotoListItem = props.photo;
 
   return (
-    <div>
-      <img src={sampleDataForPhotoListItem.imageSource} alt="test image" />
-      <img src={sampleDataForPhotoListItem.profile} alt="profile pic" />
+<div className="photo-list__item">
+  <img src={sampleDataForPhotoListItem.imageSource} alt="test image" />
+  <img src={sampleDataForPhotoListItem.profile} alt="profile pic" />
+  
+  <div className="photo-details">
     <span>{sampleDataForPhotoListItem.username}</span>
+  </div>
+  
+  <div className="photo-location">
     <span>{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</span>
   </div>
+</div>
   );
 };
 
