c20804bb67c5b1968bba0ab66203ac0b3d9d7733
amusictheorist <a_millaresthomson@outlook.com>
2024-11-01 16:15:06 -0400

    Implement HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import PhotoList from 'components/PhotoList';
-import TopNavigationBar from 'components/TopNavigationBar';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 const App = () => {
  
   return (
     <div className="App">
-    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <TopNavigationBar />
-    <PhotoList />
+      <HomeRoute />
   </div>
   );
 };
