0894f75f1d464eda917b9eeaddb84b0cb8aed59f
amusictheorist <a_millaresthomson@outlook.com>
2024-11-02 13:35:25 -0400

    Implement Modal Window

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics} favoritedPhotos={favoritedPhotos} toggleFavorite={toggleFavorite}/>
+      <PhotoDetailsModal />
   </div>
   );
 };
