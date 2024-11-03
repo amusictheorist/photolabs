5a4b62741e9b1bdeef00be54d01b13d5ea46ddf9
amusictheorist <a_millaresthomson@outlook.com>
2024-11-03 10:27:02 -0500

    Implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,31 +1,65 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  favoritedPhotos: [],
+  displayModal: false,
+  activePhoto: null,
+};
+
+export const ACTIONS = {
+  TOGGLE_FAVORITE: 'TOGGLE_FAVORITE',
+  TOGGLE_MODAL: 'TOGGLE_MODAL',
+  OPEN_MODAL_WITH_PHOTO: 'OPEN_MODAL_WITH_PHOTO'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_FAVORITE":
+      return {
+        ...state,
+        favoritedPhotos: state.favoritedPhotos.includes(action.photoId)
+          ? state.favoritedPhotos.filter((id) => id !== action.photoId)
+          : [...state.favoritedPhotos, action.photoId],
+      };
+    case "TOGGLE_MODAL":
+      return {
+        ...state,
+        displayModal: !state.displayModal,
+      };
+    case "OPEN_MODAL_WITH_PHOTO":
+      return {
+        ...state,
+        displayModal: true,
+        activePhoto: action.photo,
+      };
+    default:
+      return state;
+  }
+};
 
 const useApplicationData = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
-  const [displayModal, setDisplayModal] = useState(false);
-  const [activePhoto, setActivePhoto] = useState(null);
-  
   const toggleFavorite = (photoId) => {
-    setFavoritedPhotos((prevFavorites) => {
-      if (prevFavorites.includes(photoId)) {
-        return prevFavorites.filter((id) => id !== photoId);
-      } else {
-        return [...prevFavorites, photoId];
-      }
-    });
-  };
-  
-  const toggleModal = () => {
-    setDisplayModal(!displayModal);
-  };
-  
-  const openModalWithPhoto = (photo) => {
-    setDisplayModal(!displayModal);
-    setActivePhoto(photo);
+    dispatch({ type: ACTIONS.TOGGLE_FAVORITE, photoId });
   };
 
-  return { toggleFavorite, toggleModal, openModalWithPhoto, favoritedPhotos, activePhoto, displayModal };
+  const toggleModal = () => {
+    dispatch({ type: ACTIONS.TOGGLE_MODAL });
+  };
+
+  const openModalWithPhoto = (photo) => {
+    dispatch({ type: ACTIONS.OPEN_MODAL_WITH_PHOTO, photo });
+  };
+
+  return {
+    toggleFavorite,
+    toggleModal,
+    openModalWithPhoto,
+    favoritedPhotos: state.favoritedPhotos,
+    activePhoto: state.activePhoto,
+    displayModal: state.displayModal,
+  };
 };
 
 export default useApplicationData;
